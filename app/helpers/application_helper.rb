eeeb473db76eb6a076d82aac2dc70b20375bfe5a
danrayfr <danrayrollan98@gmail.com>
2024-09-19 06:07:20 +0800

    Add to display encrypted text/csv data in record show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def encrypt_data(data)
+    cipher = OpenSSL::Cipher::AES.new(128, :CBC)
+    cipher.encrypt
+    key = cipher.random_key
+    iv = cipher.random_iv
+    encrypted = cipher.update(data) + cipher.final
+    Base64.encode64(encrypted) # Encode the encrypted data to make it easier to display
+  end
 end
